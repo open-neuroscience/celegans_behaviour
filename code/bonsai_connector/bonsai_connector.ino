a37100ef4c604f43a80220b34caa065a05ad9813
Andre M Chagas <andremchagas@proton.me>
2026-08-25 12:16:41 -0300

    start bonsai pipeline

--- code/bonsai_connector/bonsai_connector.ino
+++ code/bonsai_connector/bonsai_connector.ino
@@ -1,9 +1,11 @@
 #include <SerialCommand.h>
 #include <Servo.h>
+
 #define blueChannel 11
 #define IRChannel 10
 #define servoChannel 9
 #define potChannel A0
+
 int potValue = 0;
 int IRValue = 0;
 
@@ -12,8 +14,8 @@ Servo tapServo;  // create servo object to control the servo that will produce t
 
 void setup() {
   Serial.begin(9600);
-  sCmd.addCommand("Opto_on",    BLED_on);          // Turns LED on to test communication, turns the onboard led on.
-  sCmd.addCommand("Opto_off",   BLED_off);         // Turns LED off
+  sCmd.addCommand("opto_on",    BLED_on);          // Turns LED on to test communication, turns the onboard led on.
+  sCmd.addCommand("opto_off",   BLED_off);         // Turns LED off
   sCmd.addCommand("servo",   servo);               // set servo position
   tapServo.attach(servoChannel); //attach pin X to servo motor
   pinMode(blueChannel, OUTPUT);
@@ -25,14 +27,14 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  //sCmd.readSerial();     // We don't do much, just process serial commands
+  sCmd.readSerial();     // We don't do much, just process serial commands
   potValue = analogRead(potChannel);
   IRValue = map(potValue,0,1023,0,255);
   analogWrite(IRChannel,IRValue);
-  analogWrite(blueChannel, 255);
-  delay(500);
-  analogWrite(blueChannel, 0);
-  delay(100);
+  //analogWrite(blueChannel, 255);
+  //delay(500);
+  //analogWrite(blueChannel, 0);
+  //delay(100);
 
 }//end void loop
 
@@ -42,10 +44,10 @@ void BLED_on(){
 
   //Serial.println("blue intensity");
   arg = sCmd.next();
-
+  //Serial.println(arg);
   if (arg != NULL) {
     aNumber = atoi(arg);    // Converts a char string to an integer
-    aNumber = map(aNumber,0,100,0,255);
+    aNumber = map(aNumber,100,0,0,255);
     //Serial.print("First argument was: ");
     //Serial.println(aNumber);
   }
